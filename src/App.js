ca9e08d3d7bd27df277c5f02b59dd1af24fab5ef
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-10 23:26:32 +0100

    remove 1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div id="content-container">
       <div id="menu">
         <div id="title">
-          Rogier de Ruijter1
+          Rogier de Ruijter
         </div>
         <div id="projects">
           <div>

79cbea3c7972c3559ac18790f9ed93a3507c0f29
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-13 22:12:05 +0100

    add rounded courners to circle game and get low

--- src/projects/CircleGame.js
+++ src/projects/CircleGame.js
@@ -1,7 +1,7 @@
 function CircleGame() {
   return (
     <div style={{width: '100%', height: '70%'}}>
-      <iframe src="https://circle-ga.me" frameBorder="0"></iframe>
+      <iframe src="https://circle-ga.me" frameBorder="0" style={{borderRadius : '10px'}}></iframe>
     </div>
   );
 }
--- src/projects/GetLow.js
+++ src/projects/GetLow.js
@@ -1,7 +1,7 @@
 function GetLow() {
   return (
     <div style={{width: '100%', height: '70%'}}>
-      <iframe src="https://rogierderuijter.github.io/get-low/" frameBorder="0"></iframe>
+      <iframe src="https://rogierderuijter.github.io/get-low/" frameBorder="0" style={{borderRadius : '10px'}}></iframe>
     </div>
   );
 }

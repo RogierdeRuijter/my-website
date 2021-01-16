d7d09c106286993bd5735e58b81b8b0ac0c4019b
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-16 17:44:32 +0100

    add sleeping seals description

--- src/projects/SleepingSeals.js
+++ src/projects/SleepingSeals.js
@@ -2,6 +2,9 @@ function SleepingTumblrSeals() {
   return (
     <div style={{width: '100%', height: '70%', paddingTop: '20px'}}>
       <iframe src="https://sleepingsealswithcelebrityquotes.com" frameBorder="0"></iframe>
+      <div style={{marginTop: '50px', fontStyle: 'italic'}}>
+      SleepingTumblrSeals. These are memes for sleeping seals. Thanks Jonna for creating this with me and eating way much candy for one night.
+      </div>
     </div>
   );
 }

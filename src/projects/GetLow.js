8b6aec501e3b57ee68ec12bd430b05fbe2634a93
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-16 17:43:37 +0100

    add get low description

--- src/projects/GetLow.js
+++ src/projects/GetLow.js
@@ -2,6 +2,9 @@ function GetLow() {
   return (
     <div style={{width: '100%', height: '80%', paddingTop: '20px'}}>
       <iframe src="https://rogierderuijter.github.io/get-low/" frameBorder="0" style={{borderRadius : '10px'}}></iframe>
+      <div style={{marginTop: '50px', fontStyle: 'italic'}}>
+        Dedication to the performance that makes me believe I can do anything. © Kanye West - All day [Brit Awards]
+      </div>
     </div>
   );
 }

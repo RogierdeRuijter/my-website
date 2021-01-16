fe0788f13725cdcb1c4644d3d1be11f84f029007
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-16 18:13:15 +0100

    add spacing to bottom

--- src/projects/SleepingSeals.js
+++ src/projects/SleepingSeals.js
@@ -2,7 +2,7 @@ function SleepingTumblrSeals() {
   return (
     <div style={{width: '100%', height: '70%', paddingTop: '20px'}}>
       <iframe src="https://sleepingsealswithcelebrityquotes.com" frameBorder="0"></iframe>
-      <div style={{marginTop: '50px', fontStyle: 'italic'}}>
+      <div style={{marginTop: '50px', paddingBottom: '20px', fontStyle: 'italic'}}>
         These are memes for sleeping seals. <br/>
         Thanks Jonna for creating this with me and eating way too much candy for one night.
       </div>

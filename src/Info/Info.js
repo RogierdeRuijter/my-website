8e59f3155172dbf9afb5753aab477b0d920d298a
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-21 16:16:15 +0100

    add varaiable widths, margins, paddings

--- src/Info/Info.js
+++ src/Info/Info.js
@@ -6,7 +6,6 @@ function Info() {
       <div>
         I am forever grateful for everyone I have worked with, thank you so much.
       </div>
-      
       <div style={{marginTop: '10px'}}>
       Special thanks:
       <ul className="special-thanks">
@@ -26,11 +25,9 @@ function Info() {
         </li>
       </ul>
       </div>
-      
       <div style={{marginTop: '20px'}}>
         And above all, thanks mom for the love, support, dedication, and drive to give us whatever we needed. 
       </div>
-      
       <div style={{marginTop: '15px'}}>
         You have made me to what I am.
       </div>
--- src/projects/Montepoeli/Montepoeli.js
+++ src/projects/Montepoeli/Montepoeli.js
@@ -8,15 +8,17 @@ function Montepoeli() {
   return (
     <>
       <div id="montepoeli-content-container" style={{width: '100%', height: '100%'}}>
-        <img className="iphone rounded-corners spacer" src={montepoeliHome}></img>
-        <img className="iphone rounded-corners spacer" src={montepoeliAddGame}></img>
-        <img className="iphone rounded-corners" src={montepoeliAddGameFilled}></img>
-        <div className="footer">
-          A website build for the chess club. 
-          The chess club was founded in Montepulciano, Italy. 
-          The website is an attempt to keep the spirit from this vacation alive. <br/>
-          It is mainly used as a PWA and is my breeding ground for exploring non functional features.
-          Semantic release, you will always be my favourite piece of software ever. Even if no one else thinks so.
+        <div>
+          <img className="iphone rounded-corners spacer" src={montepoeliHome}></img>
+          <img className="iphone rounded-corners spacer" src={montepoeliAddGame}></img>
+          <img className="iphone rounded-corners" src={montepoeliAddGameFilled}></img>
+          <div className="footer">
+            A website build for the chess club. 
+            The chess club was founded in Montepulciano, Italy. 
+            The website is an attempt to keep the spirit from this vacation alive. <br/>
+            It is mainly used as a PWA and is my breeding ground for exploring non functional features.
+            Semantic release, you will always be my favourite piece of software ever. Even if no one else thinks so.
+          </div>
         </div>
       </div>
     </>

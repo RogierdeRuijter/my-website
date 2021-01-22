1801812c1ea518aba9b2fcbcf7a026aad0d0b525
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-22 10:23:17 +0100

    scale fish based on width of screen

--- src/projects/FishRace/FishRace.js
+++ src/projects/FishRace/FishRace.js
@@ -2,7 +2,7 @@ import './FishRace.css';
 
 function FishRace() {
   return (
-    <div style={{width: '100%', height: '80%'}}>
+    <div id="fish-race-container">
       <iframe src="https://itisafishrace.xyz" frameBorder="0"></iframe>
       <div id="description">
         Created during a night filled with drinks by Daniel and me. 

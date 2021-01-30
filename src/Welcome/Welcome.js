4312e5284c1de62e48d07da08ca19c10bced3ced
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-01-30 01:05:37 +0100

    add heart

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -3,9 +3,7 @@ import './Welcome.css';
 function Welcome({welcomeTextRef}) {
   return (
     <div id="welcome-container">
-      <div ref={welcomeTextRef} style={{fontSize: '5vw'}}>
-        Welcome
-      </div>
+      <div ref={welcomeTextRef} style={{fontSize: '10vw'}}>❤️</div>
       <div id="scroll-down-element">
         <span id="scroll-down-element-line"></span>
         <span id="scroll-down-element-text">Scroll</span>

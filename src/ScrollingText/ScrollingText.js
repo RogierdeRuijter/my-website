b9b6936c37d1cf7dfc346e388cbc8919d198668d
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-05-10 16:43:16 +0200

    add more audio and sync it with the scrolling text

--- src/ScrollingText/ScrollingText.js
+++ src/ScrollingText/ScrollingText.js
@@ -1,32 +1,26 @@
 import { useState, useEffect, useLayoutEffect } from 'react';
 import { useInterval } from '../helpers/use-interval/use-interval';
 import './ScrollingText.css';
-
-// const texts = [
-//   'Thank you', 
-//   'Welcome', 
-//   'New', 
-//   'More'
-// ];
+import fishRaceAudio from '../assets/fish-race.mp3';
 
 const fishRaceDescriptions = [
   'Gemaakt in een avond vol met drank door Daniel en mij',
   'Later verfijnd door de mensen die hiervoor genoemd zijn en Joost',
   'Bedankt jongens voor de eindeloze discussies over de essentie van de Fish race' 
 ];
-
 const fishRaceDelays = [
-  2000,
-  4000,
-  5000
+  5500,
+  9500 - 5500,
+  14000 - 9500
 ];
 
+// TODO: rename component that is matches more the scrolling text plus the audio 
 function ScrollingText({project}) {
-  const [ previousTest, setPreviousText ] = useState('');
-
   // TODO: rename to descriptions
   const [ texts, setTexts ] = useState();
   const [ delays, setDelays ] = useState();
+  
+  const [ previousText, setPreviousText ] = useState('');
   const [ activeText, setActiveText ] = useState();
   const [ nextText, setNextText ] = useState();
 
@@ -35,6 +29,9 @@ function ScrollingText({project}) {
   
   // TODO: find a better name for this state variable
   const [ isRunning, setIsRunning ] = useState(false);
+
+  const [audio] = useState(new Audio(fishRaceAudio));
+  
 
   useInterval(() => {
     setPreviousText(texts[currentIndex])
@@ -72,13 +69,24 @@ function ScrollingText({project}) {
 
   return (
     <>
+      {/* TODO: add pause and play functionallity
+            Implementation used https://stackoverflow.com/questions/47686345/playing-sound-in-react-js */}
       <button onClick={() => {
         setIsRunning(true);
         
         setActiveText(texts[0]);
         setNextText(texts[1]);
-        }}>Start</button>
-      <p className="previous-text">{previousTest}</p>
+
+        // For reset
+        if (currentDelay === undefined) {
+          setPreviousText(undefined)
+          setCurrentDelay(delays[0]);
+          setCurrentIndex(0);
+        }
+
+        audio.play();
+        }}>Play</button>
+      <p className="previous-text">{previousText}</p>
       <p className="active-text">{activeText}</p>
       <p className="next-text">{nextText}</p>
     </>
--- src/ScrollingText/ScrollingText.test.js
+++ src/ScrollingText/ScrollingText.test.js
@@ -5,7 +5,8 @@ import { act } from 'react-dom/test-utils';
 
 describe('ScrollingText', () => {
   beforeEach(() => {
-    jest.useFakeTimers()
+    jest.useFakeTimers();
+    window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
   });
 
   afterEach(() => {
@@ -13,15 +14,7 @@ describe('ScrollingText', () => {
     jest.useRealTimers()
   });
 
-  it.only('should show the fish race description in order', () => {
-    render(<ScrollingText project="fish-race"/>);
-
-    const firstSentence_0 = screen.getByText('Gemaakt in een avond vol met drank door Daniel en mij');
-    expect(firstSentence_0).toBeInTheDocument();
-    expect(firstSentence_0).toHaveClass('next-text');
-
-    userEvent.click(screen.getByText('Start'));
-
+  const testFishRaceFlow = () => {
     const firstSentence_1 = screen.getByText('Gemaakt in een avond vol met drank door Daniel en mij');
     expect(firstSentence_1).toBeInTheDocument();
     expect(firstSentence_1).toHaveClass('active-text');
@@ -30,7 +23,7 @@ describe('ScrollingText', () => {
     expect(secondSentence_1).toBeInTheDocument();
     expect(secondSentence_1).toHaveClass('next-text');
 
-    act(() => jest.advanceTimersByTime(2000));
+    act(() => jest.advanceTimersByTime(5500));
 
     const secondSentence_2 = screen.getByText('Later verfijnd door de mensen die hiervoor genoemd zijn en Joost');
     expect(secondSentence_2).toBeInTheDocument();
@@ -40,17 +33,45 @@ describe('ScrollingText', () => {
     expect(thirdSentence_2).toBeInTheDocument();
     expect(thirdSentence_2).toHaveClass('next-text');
 
-    act(() => jest.advanceTimersByTime(4000));
+    act(() => jest.advanceTimersByTime(9500 - 5500));
 
     const thirdSentence_3 = screen.getByText('Bedankt jongens voor de eindeloze discussies over de essentie van de Fish race');
     expect(thirdSentence_3).toBeInTheDocument();
     expect(thirdSentence_3).toHaveClass('active-text');
 
-    act(() => jest.advanceTimersByTime(5000));
+    act(() => jest.advanceTimersByTime(14000 - 9500));
 
     const thirdSentence_4 = screen.getByText('Bedankt jongens voor de eindeloze discussies over de essentie van de Fish race');
     expect(thirdSentence_4).toBeInTheDocument();
     expect(thirdSentence_4).toHaveClass('previous-text');
+  }
+
+  it.only('should show the fish race description in order', () => {
+    render(<ScrollingText project="fish-race"/>);
+
+    const firstSentence_0 = screen.getByText('Gemaakt in een avond vol met drank door Daniel en mij');
+    expect(firstSentence_0).toBeInTheDocument();
+    expect(firstSentence_0).toHaveClass('next-text');
+
+    userEvent.click(screen.getByRole('button', {name: 'Play'}));
+    
+    testFishRaceFlow();
+  });
+
+  it.only('should be able to restart the fish race description when after it ends', () => {
+    render(<ScrollingText project="fish-race"/>);
+
+    const firstSentence_0 = screen.getByText('Gemaakt in een avond vol met drank door Daniel en mij');
+    expect(firstSentence_0).toBeInTheDocument();
+    expect(firstSentence_0).toHaveClass('next-text');
+
+    userEvent.click(screen.getByRole('button', {name: 'Play'}));
+    
+    testFishRaceFlow();
+
+    userEvent.click(screen.getByRole('button', {name: 'Play'}));
+
+    testFishRaceFlow();
   });
 });
 

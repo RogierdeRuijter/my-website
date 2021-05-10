c6e7064d1537861e3172fbf1744769ee927cd101
Rogier De Ruijter <rogierderuijter@hotmail.com>
2021-05-10 17:03:29 +0200

    run prettier

--- src/Welcome/Welcome.js
+++ src/Welcome/Welcome.js
@@ -1,10 +1,10 @@
-import './Welcome.css';
+import "./Welcome.css";
 
-function Welcome({welcomeTextRef, children}) {
+function Welcome({ welcomeTextRef, children }) {
   return (
     <div id="welcome-container">
-      <div ref={welcomeTextRef} style={{fontSize: '4vw'}}>
-        { children }
+      <div ref={welcomeTextRef} style={{ fontSize: "4vw" }}>
+        {children}
       </div>
       <div id="scroll-down-element">
         <span id="scroll-down-element-line"></span>
